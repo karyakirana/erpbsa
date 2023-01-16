b7dbaafa3326953106f8fd0a92bdd20b6c4872d9
karyakirana <adibpratama2@gmail.com>
2023-01-17 03:27:07 +0700

    update

---
+++ database/migrations/2023_01_17_032135_create_closedcash_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('close_cash', function (Blueprint $table) {
+            $table->id();
+            $table->string('active')->unique();
+            $table->string('closed')->nullable()->unique();
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('close_cash');
+    }
+};
