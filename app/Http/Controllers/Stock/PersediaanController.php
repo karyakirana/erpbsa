a6597197f84ab490772d60318ecf4423a3877fff
karyakirana <adibpratama2@gmail.com>
2023-01-17 03:20:47 +0700

    update

--- app/Http/Controllers/Stock/PersediaanController.php
+++ app/Http/Controllers/Stock/PersediaanController.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers\Stock;
 
 use App\Http\Controllers\Controller;
+use App\Models\Stock\Persediaan;
 use Illuminate\Http\Request;
 
 class PersediaanController extends Controller
 {
-    //
+    public function getData()
+    {
+        try {
+            $persediaan = Persediaan::with(['produk'])
+                ->where('active_cash', session('ClosedCash'))
+                ->get();
+            return response()->json([
+                'status' => true,
+                'data' => $persediaan
+            ], 200);
+        } catch (\Exception $e){
+            return response()->json([
+                'status' => false,
+                'messages' => $e->getMessage()
+            ], 403);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -72,6 +72,8 @@ Route::middleware('auth:sanctum')->group(function (){
 });
 
 Route::middleware('auth:sanctum')->group(function (){
+    // persediaan
+    Route::get('persediaan', [\App\Http\Controllers\Stock\PersediaanController::class, 'getData']);
     // persediaan awal
     Route::get('persediaan/awal/{persediaan_awal_id}', [\App\Http\Controllers\Stock\PersediaanAwalController::class, 'edit']);
     Route::post('persediaan/awal/', [\App\Http\Controllers\Stock\PersediaanAwalController::class, 'store']);
