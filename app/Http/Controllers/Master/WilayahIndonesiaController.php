0a638a56f8e87de862c26bd899fd6876614770b2
karyakirana <adibpratama2@gmail.com>
2023-01-17 02:55:08 +0700

    update

--- app/Http/Controllers/Master/WilayahIndonesiaController.php
+++ app/Http/Controllers/Master/WilayahIndonesiaController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\Master;
 
 use App\Http\Controllers\Controller;
+use App\Models\Regency;
 use Illuminate\Http\Request;
 
 class WilayahIndonesiaController extends Controller
 {
-    //
+    public function kotaIndonesia()
+    {
+        try {
+            $kota = Regency::with('province');
+            return response()->json([
+                'status' => true,
+                'data' => $kota
+            ], 200);
+        } catch (\Exception $e){
+            return response()->json([
+                'status' => false,
+                'messages' => $e->getMessage()
+            ], 403);
+        }
+    }
 }
