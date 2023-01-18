45f2e3b0d54d2634254a02042aeb13af53da1d04
karyakirana <adibpratama2@gmail.com>
2023-01-18 23:54:32 +0700

    update

--- app/Models/Master/Customer.php
+++ app/Models/Master/Customer.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Customer extends Model
 {
-    use HasFactory, SoftDeletes, KodeTrait;
+    use HasFactory, KodeTrait;
     protected $table = 'customer';
     protected $fillable = [
         'kode',
