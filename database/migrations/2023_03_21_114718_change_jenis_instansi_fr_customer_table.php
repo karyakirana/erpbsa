9b79d698416bf9625e493064339558dc195e9192
karyakirana <adibpratama2@gmail.com>
2023-03-21 11:52:17 +0700

    update from mac

---
+++ database/migrations/2023_03_21_114718_change_jenis_instansi_fr_customer_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customer', function (Blueprint $table) {
+            $table->dropColumn('jenis_instansi');
+            $table->unsignedBigInteger('jenis_instansi_id')->after('kode');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customer', function (Blueprint $table) {
+            $table->string('jenis_instansi')->after('kode');
+            $table->dropColumn('jenis_instansi');
+        });
+    }
+};
